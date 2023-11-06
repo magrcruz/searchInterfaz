cd641a36fcab92dfaea9d5d4028c246815f8cb92
Alan Monroy Bernedo <alan.monroy@ucsp.edu.pe>
2023-11-06 08:06:46 -0500

    mide tiempos

--- src/routes/search/+page.server.js
+++ src/routes/search/+page.server.js
@@ -3,18 +3,25 @@ import { supabase } from "$lib/client.js"
 export async function load({ url }) {
     let query = url.searchParams.get("query").toLowerCase().trim()
 
+    // Iniciar el cronómetro
+    const startTime = performance.now();
 
     let { data, error } = await supabase
         .rpc('search_pages', {
             term_to_search: query
         })
 
+    // Detener el cronómetro
+    const endTime = performance.now();
+
+    // Calcular el tiempo total en segundos
+    const timeTaken = ((endTime - startTime) / 1000).toFixed(2);
+
     if (error) console.error(error)
     else console.log(data)
 
-
     return {
-        time: Math.random().toFixed(2),
+        time: timeTaken, // tiempo en segundos
         pages: data
     }
 }
